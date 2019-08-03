7c141c03a9e87439321754157ce7841333d23fbd
MICHAEL DUVAL <duv74@hotmail.com>
2019-08-03 15:48:49 -0400

    error fix

--- sql_duval_queries.sql
+++ sql_duval_queries.sql
@@ -82,7 +82,7 @@ WHERE
 -- TASK #8
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-SELECT first_name, COUNT(first_name) AS "actor count"
-FROM actor
-GROUP BY first_name
-ORDER BY "actor count" DESC;
+SELECT last_name, COUNT(last_name) AS "last name count"
+FROM employees
+GROUP BY last_name
+ORDER BY "last name count" DESC;
